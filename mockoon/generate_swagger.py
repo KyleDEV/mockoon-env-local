ad4f33177c5af18a80353991619a52045f02f87b
KyleDEV <unilook@me.com>
2024-06-22 10:37:22 +0900

    swagger 스펙생성시 Mocking의 response를 더 잘 반영하도록 수정

--- mockoon/generate_swagger.py
+++ mockoon/generate_swagger.py
@@ -16,20 +16,29 @@ def generate_swagger(mockoon_data):
         responses = {}
 
         for response in route["responses"]:
-            # Assuming response body is a JSON object
+            # Assuming response body is a JSON object or faker template
             try:
                 response_body = json.loads(response["body"])
             except json.JSONDecodeError:
-                response_body = {}
+                response_body = response["body"]  # Keeping the body as string if not JSON
+
+            # Creating the response content schema
+            if isinstance(response_body, dict):
+                schema = {
+                    "type": "object",
+                    "example": response_body
+                }
+            else:
+                schema = {
+                    "type": "string",
+                    "example": response_body
+                }
 
             responses[str(response["statusCode"])] = {
                 "description": response["label"],
                 "content": {
                     "application/json": {
-                        "schema": {
-                            "type": "object",
-                            "example": response_body
-                        }
+                        "schema": schema
                     }
                 }
             }
@@ -41,6 +50,19 @@ def generate_swagger(mockoon_data):
             "summary": route["documentation"],
             "responses": responses
         }
+
+        # Add request body if available
+        if route["responses"]:
+            request_body = {
+                "content": {
+                    "application/json": {
+                        "schema": {
+                            "type": "object"
+                        }
+                    }
+                }
+            }
+            swagger["paths"][path][method]["requestBody"] = request_body
 
     return swagger
 
